6820ef1f52b91b426efd41087c85542e0c83b334
Daniel <danleavitt0@gmail.com>
2016-06-30 19:53:09 -0700

    function finder

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var app = require('express')()
 var path = require('path')
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + req.path))
+  res.sendFile(path.join(__dirname, req.path))
 })
 
 app.listen(process.env.PORT || 3000)
